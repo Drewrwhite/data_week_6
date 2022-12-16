330eec80d5bc1ea5f1ad7b20d4341bae2c7b9320
Drew White <d.white0002@gmail.com>
2022-12-16 08:49:52 -0800

    complete second query

--- code_review.sql
+++ code_review.sql
@@ -3,3 +3,6 @@
 -- Show only names that include the substring 'Hair', and order them with the part_num in descending order
 SELECT name, num_parts FROM sets WHERE name LIKE '%Hair%' ORDER BY num_parts DESC;
 
+-- From the sets table, select all the sets that included the word 'Showdown' in the name between 1990 and 2015
+SELECT * FROM sets WHERE name LIKE '%Showdown%' AND year BETWEEN 1990 AND 2015;
+
