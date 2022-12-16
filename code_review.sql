526000e093dab2c0705f5be4335791cb707445a3
Drew White <d.white0002@gmail.com>
2022-12-16 09:02:31 -0800

    complete fifth query

--- code_review.sql
+++ code_review.sql
@@ -12,4 +12,8 @@ SELECT id, name FROM part_categories AS p WHERE name LIKE '%Bricks%';
 -- Subqueries
 -- Select all the ids from the themes table with 'Pirates' or 'Star Wars' in the name. This is your subquery. 
 -- Then show the names of all the sets where the theme_id matches an id in that subquery.
-SELECT * FROM sets WHERE theme_id IN (SELECT id FROM themes WHERE name LIKE '%Pirates%' OR name LIKE '%Star Wars%');
+SELECT name FROM sets WHERE theme_id IN (SELECT id FROM themes WHERE name LIKE '%Pirates%' OR name LIKE '%Star Wars%');
+
+-- select the ids of the values in the inventories table that have more than one version (i.e. version > 1). This is your subquery.
+-- Then select everything from the inventory_parts table where the inventory_id matches an id in that subquery. Limit the output to 10 rows.
+SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1);
