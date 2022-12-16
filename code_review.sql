b99a36e83fd8c3f6af7cba897c8b6e4b9807eab7
Drew White <d.white0002@gmail.com>
2022-12-16 09:51:40 -0800

    edit query 1

--- code_review.sql
+++ code_review.sql
@@ -1,7 +1,7 @@
 -- Selecting values
--- Select the name and num_parts columns from the parts table. 
+-- Select the name and part_num columns from the parts table. 
 -- Show only names that include the substring 'Hair', and order them with the part_num in descending order
-SELECT name, num_parts FROM sets WHERE name LIKE '%Hair%' ORDER BY num_parts DESC;
+SELECT name, part_num FROM parts WHERE name LIKE '%Hair%' ORDER BY part_num DESC;
 
 -- From the sets table, select all the sets that included the word 'Showdown' in the name between 1990 and 2015
 SELECT * FROM sets WHERE name LIKE '%Showdown%' AND year BETWEEN 1990 AND 2015;
